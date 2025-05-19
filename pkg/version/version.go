1686cf1dd1dd2c7f34fd66e7fb265dbe7221d944
Alon Ohana <alon.o@sealsecurity.io>
2025-05-19 10:52:12 +0300

    bld2

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -5,7 +5,7 @@ import "strings"
 var (
 	Program      = "k3s"
 	ProgramUpper = strings.ToUpper(Program)
-	Version      = "dev"
+	Version      = "1.32.3-sp1"
 	GitCommit    = "HEAD"
 
 	UpstreamGolang = ""
